3dae956503b810fbb6cbefe89db7b807db3a42eb
Nicholas Gunter <ygnick@live.com>
2019-07-16 16:23:15 -0400

    added average vote searching and accounted for unhappy cases

--- index.js
+++ index.js
@@ -6,36 +6,66 @@ server.use(morgan('dev'));
 require('dotenv').config();
 
 
-console.log(process.env.API_TOKEN);
+function getGenre(arr){
+  let newArr = arr.map(item => item['genre']);
+  return newArr;
+}
+function getCountry(arr){
+  let newArr = arr.map(item => item['country']);
+  return newArr;
+}
+function getVotes(arr){
+  let newArr = arr.map(item => Number(item['avg_vote']));
+  return newArr;
+}
 
-// function titlize(str){
-//   return str.charAt(0).tolowerCase() + str.slice(1);
-// }
+let genreArr = getGenre(movies);
+let countryArr = getCountry(movies);
+let voteArr = getVotes(movies);
+
 
 server.get('/movie', (req, res) => {
- const {genre, country, avg_vote} = req.query;
+  const { genre, country, avg_vote } = req.query;
+
   let filteredMovies = [...movies];
 
-  if(genre){
+  if (!genre && !country && !avg_vote) {
+    return res.status(400).json({error: 'Please search by country, genre or avg_vote.'})
+  }
+
+  if (genre && !genreArr.includes(genre)){
+    return res.status(400).json({error: 'Please enter valid genre. ex: action'});
+  }
+
+  if (country && !countryArr.includes(country)){
+    return res.status(400).json({error: 'Please enter valid country. ex: United States'});
+  }
+
+  if (avg_vote && !(avg_vote > 0 && avg_vote <= 10)){
+    return res.status(400).json({error: 'Please enter valid avg_vote. (Between 1 - 10) ex: 6.4'});
+  }
+
+  if (genre) {
     let convertedGenre = genre.toLowerCase();
-
-    filteredMovies= filteredMovies.filter(movie => movie["genre"].toLowerCase().includes(convertedGenre));
-
+    filteredMovies = filteredMovies.filter(movie => movie['genre'].toLowerCase().includes(convertedGenre));
   }
-  
-  if(country){
+
+  if (country) {
     let convertedCountry = country.toLowerCase();
+    filteredMovies = filteredMovies.filter(movie => movie['country'].toLowerCase().includes(convertedCountry));
+  }
 
-    filteredMovies= filteredMovies.filter(movie => movie["country"].toLowerCase().includes(convertedCountry));
-
+  if (avg_vote) {
+    let convertedVote = Number(avg_vote);
+    console.log(avg_vote);
+    filteredMovies = filteredMovies.filter(movie => Number(movie['avg_vote']) >= convertedVote);
   }
 
 
 
 
 
- res.json(filteredMovies);
+  res.json(filteredMovies);
 });
 
-server.listen(8080, () => console.log('Server on 8080 is running'))
-  
+server.listen(8080, () => console.log('Server on 8080 is running'));
